af8b47f1dbb793f01da5e1386eff6efc2fb54b1c
Mustafa UYSAL <mustafa-uysal@outlook.com>
2023-11-12 21:09:21 +0300

    add models and controllers

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\ApiAuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +12,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    
+    Route::post('/login', [ApiAuthController::class, 'login'])->name('login.api');
+    Route::post('/register',[ApiAuthController::class, 'register'])->name('register.api');
+    
+    Route::middleware('auth:api')->group(function () {
+        // our routes to be protected will go in here
+        Route::post('/logout', [ApiAuthController::class, 'logout'])->name('logout.api');
+    });
+
 });
