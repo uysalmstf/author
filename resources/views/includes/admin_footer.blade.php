ad52a39aec71232c9cfb7243bb0bef59a2a1f85a
Mustafa UYSAL <mustafa-uysal@outlook.com>
2023-11-26 16:25:31 +0300

    admin layout parçalama

---
+++ resources/views/includes/admin_footer.blade.php
@@ -0,0 +1,16 @@
+</div>
+</div>
+</body>
+<script
+    src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-1.13.8/af-2.6.0/b-2.4.2/b-colvis-2.4.2/b-html5-2.4.2/b-print-2.4.2/cr-1.7.0/date-1.5.1/fc-4.3.0/fh-3.4.0/kt-2.11.0/r-2.5.0/rg-1.4.1/rr-1.4.1/sc-2.3.0/sb-1.6.0/sp-2.2.0/sl-1.7.0/sr-1.3.0/datatables.min.js"></script>
+<script>
+
+
+</script>
+</html>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,3 @@
+@include('includes.admin_header')
+            @yield('content')
+ @include('includes.admin_footer')
